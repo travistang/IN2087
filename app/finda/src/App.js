27e2f53e4a96dae599a4138e74c3d1547c6a79c5
Travis Tang <ga86yam@mytum.de>
2018-05-28 07:37:35 +0200

    Prototypes of the navbar

--- app/finda/src/App.js
+++ app/finda/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
+import { Router, Route, Link,BrowserRouter} from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+import Ads from './components/Ads/Ads'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+            <NavBar username="Hello World!"/>
+            <section className="Section">
+              <div class="row" >
+                <Ads/>
+                <div class="col" style={{marginLeft:16,marginRight:16}}>
+                  <div class="row" style={{marginBottom:8}}>
+
+                      <Route path="/me" component={Ads}>
+                      </Route>
+
+                  </div>
+                </div>
+                <Ads/>
+              </div>
+            </section>
+
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ app/finda/src/components/Ads/Ads.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+export default function(props) {
+  return (
+    <div class="col col-2" style={{background: "gray"}}>
+    </div>
+  )
+}
