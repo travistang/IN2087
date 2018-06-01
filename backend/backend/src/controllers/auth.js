9ee40b2819a8329d24bbece4946a5e597cedc0f8
travis <ga86yam@mytum.de>
2018-06-01 12:58:39 +0200

    Added frontend to docker-compose; but is not running atm; fixed a big during registration

--- backend/backend/src/controllers/auth.js
+++ backend/backend/src/controllers/auth.js
@@ -43,7 +43,9 @@ const login = (req,res) => {
 
 
 const register = (req,res) => {
-    for (let expectedAttribute in 'username password gender dob'.split(' ')) {
+    let reqAttributes =  'username password gender dob'.split(' ')
+    for (let i in reqAttributes) {
+      let expectedAttribute = reqAttributes[i]
       if (!Object.prototype.hasOwnProperty.call(req.body, 'password')) return res.status(400).json({
           error: 'Bad Request',
           message: `The request body must contain a ${expectedAttribute} property`
